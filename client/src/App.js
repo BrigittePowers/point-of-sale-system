0c06706de60ea505bdd766b56eb78f010cbee4b6
BrigittePowers <reyhn701@gmail.com>
2022-01-08 11:33:39 -0600

    Recompiled client side code to test heroku plugin

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+	request: (operation) => {
+		const token = localStorage.getItem('id_token');
+
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : '',
+			},
+		});
+	},
+	uri: '/graphql',
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path='/' component={SearchBooks} />
+						<Route exact path='/saved' component={SavedBooks} />
+						<Route
+							render={() => (
+								<h1 className='display-2'>Wrong page!</h1>
+							)}
+						/>
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,74 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+	mutation loginUser($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+			}
+		}
+	}
+`;
+
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $password: String!, $email: String!) {
+		addUser(username: $username, password: $password, email: $email) {
+			user {
+				_id
+				username
+				email
+				bookCount
+				savedBooks {
+					authors
+					bookId
+					image
+					link
+					title
+					description
+				}
+			}
+			token
+		}
+	}
+`;
+
+export const SAVE_BOOK = gql`
+	mutation saveBook($input: savedBook!) {
+		saveBook(input: $input) {
+			_id
+			username
+			email
+			bookCount
+			savedBooks {
+				# _id
+				bookId
+				authors
+				image
+				link
+				title
+				description
+			}
+		}
+	}
+`;
+
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId: ID!) {
+		removeBook(bookId: $bookId) {
+			_id
+			username
+			email
+			bookCount
+			savedBooks {
+				# _id
+				bookId
+				authors
+				image
+				link
+				title
+				description
+			}
+		}
+	}
+`;
