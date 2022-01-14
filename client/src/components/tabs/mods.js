869421a126c6da34a1f5d81392662e18e9b82f76
BrigittePowers <reyhn701@gmail.com>
2022-01-13 21:56:28 -0600

    Added data-driven sections to mods page

--- client/src/components/tabs/mods.js
+++ client/src/components/tabs/mods.js
@@ -1,31 +1,54 @@
 import React from 'react';
 
-export default function mods({ items, currentCat, handleTabChange }) {
+export default function mods({
+	items,
+	currentCat,
+	currentTab,
+	handleTabChange,
+}) {
+	function generateSections(opt) {
+		let types = [];
 
-	//TODO: Sort 
+		// const unique = [...new Set(types)];
 
-	console.log(currentCat);
-	let categories = []
-	categories = currentCat.forEach(obj => {
-		// console.log(obj.category);
-		categories.push(obj.category);
-		console.log(currentCat);
-		return currentCat;
-	});
-	console.log(categories);
-	// let categories = currentCat.array.forEach((obj) => {
-	// 	console.log(obj);
-	// });
-	// let unique = [...new Set(categories)];
+		opt.map((opt) => {
+			let typeList = types.push(opt.type);
+			return typeList;
+		});
+
+		let unique = [...new Set(types)];
+
+		return unique;
+
+		// console.log(cats);
+	}
+
+	let sections = generateSections(currentCat);
 
 	return (
 		<div>
-			{currentCat.map((opt) => (
+			{sections.map((sec) => (
+				<div key={sec}>
+					<h1>{sec}</h1>
+					{currentCat.map((opt) => (
+						<div key={opt._id}>
+							{opt.type === sec && (
+								<button onClick={() => handleTabChange('Mods')}>
+									<div>{opt.name}</div>
+									{opt.adjust > 0 && <div>{opt.adjust}</div>}
+								</button>
+							)}
+						</div>
+					))}
+				</div>
+			))}
+
+			{/* {currentCat.map((opt) => (
 				<button key={opt._id} onClick={() => handleTabChange('Mods')}>
 					<div>{opt.name}</div>
-					<div>{/* {if (opt.adjust)}S */}</div>
+					{opt.adjust > 0 && <div>{opt.adjust}</div>}
 				</button>
-			))}
+			))} */}
 			<button onClick={() => handleTabChange('Menu')}>Return</button>
 		</div>
 	);
